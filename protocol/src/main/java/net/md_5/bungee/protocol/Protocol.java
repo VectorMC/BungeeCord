9c83c10588c5d8d326dd1dd4e633591088aae078
Jedediah Smith <jedediah@silencegreys.com>
2014-11-30 15:02:58 -0500

    A few more packet ID range checks

--- protocol/src/main/java/net/md_5/bungee/protocol/Protocol.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/Protocol.java
@@ -357,7 +357,7 @@ public enum Protocol
             {
                 throw new BadPacketException( "Unsupported protocol version" );
             }
-            if ( id > MAX_PACKET_ID )
+            if ( id < 0 || id > MAX_PACKET_ID )
             {
                 throw new BadPacketException( "Packet with id " + id + " outside of range " );
             }
