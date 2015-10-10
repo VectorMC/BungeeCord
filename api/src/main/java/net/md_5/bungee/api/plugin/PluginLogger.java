01e0c69aeffbe6a4b25d9a5013bae9022452d800
Jedediah Smith <jedediah@silencegreys.com>
2015-10-10 18:38:52 -0400

    Register PluginLoggers with the LogManager

--- api/src/main/java/net/md_5/bungee/api/plugin/PluginLogger.java
+++ api/src/main/java/net/md_5/bungee/api/plugin/PluginLogger.java
@@ -1,10 +1,29 @@
 package net.md_5.bungee.api.plugin;
 
+import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
 
 public class PluginLogger extends Logger
 {
+    public static PluginLogger get(Plugin context) {
+        LogManager lm = LogManager.getLogManager();
+        Logger logger = lm.getLogger(context.getClass().getCanonicalName());
+
+        if(logger instanceof PluginLogger) {
+            return (PluginLogger) logger;
+        } else {
+            PluginLogger pluginLogger = new PluginLogger(context);
+
+            // Register the logger under the plugin's name, unless some other logger is already using the name
+            if(logger == null) {
+                lm.addLogger(pluginLogger);
+                pluginLogger.setParent(context.getProxy().getLogger()); // addLogger changes this, change it back
+            }
+
+            return pluginLogger;
+        }
+    }
 
     private final String pluginName;
 
