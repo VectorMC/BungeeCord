bec77722b45ef4aa773c3c7e1974c663dff834c0
Jedediah Smith <jedediah@silencegreys.com>
2015-10-10 18:38:52 -0400

    Register PluginLoggers with the LogManager

--- log/src/main/java/net/md_5/bungee/log/BungeeLogger.java
+++ log/src/main/java/net/md_5/bungee/log/BungeeLogger.java
@@ -31,7 +31,6 @@ public class BungeeLogger extends Logger
             addHandler( fileHandler );
 
             ColouredWriter consoleHandler = new ColouredWriter( reader );
-            consoleHandler.setLevel( Level.INFO );
             consoleHandler.setFormatter( formatter );
             addHandler( consoleHandler );
         } catch ( IOException ex )
