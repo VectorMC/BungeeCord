549bb421a59af8b8d7fe0629f998e47550f8f037
Jedediah Smith <jedediah@silencegreys.com>
2017-01-04 18:37:00 -0500

    Stringify any non-component passed to TranslatableComponent with list

--- chat/src/main/java/net/md_5/bungee/api/chat/TranslatableComponent.java
+++ chat/src/main/java/net/md_5/bungee/api/chat/TranslatableComponent.java
@@ -76,11 +76,9 @@ public class TranslatableComponent extends BaseComponent
             List<BaseComponent> temp = new ArrayList<BaseComponent>();
             for ( Object w : with )
             {
-                if ( w instanceof String )
-                {
-                    temp.add( new TextComponent( (String) w ) );
-                } else
-                {
+                if(w instanceof BaseComponent) {
+                    temp.add((BaseComponent) w);
+                } else {
                     temp.add( (BaseComponent) w );
                 }
             }
